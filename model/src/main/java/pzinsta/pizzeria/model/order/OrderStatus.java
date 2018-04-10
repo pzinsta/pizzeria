db98c03ec08c26c0b6ece304d78265f833d8e2db
pzinsta <voodooshake@gmail.com>
2018-04-10 22:38:10 +0300

    [F] Add additional order status

--- model/src/main/java/pzinsta/pizzeria/model/order/OrderStatus.java
+++ model/src/main/java/pzinsta/pizzeria/model/order/OrderStatus.java
@@ -1,5 +1,5 @@
 package pzinsta.pizzeria.model.order;
 
 public enum OrderStatus {
-	NOT_PAID, PAID, READY, BEING_DELIVERED, DELIVERED
+	NOT_PAID, PAID, BEING_MADE, READY, BEING_DELIVERED, DELIVERED
 }
