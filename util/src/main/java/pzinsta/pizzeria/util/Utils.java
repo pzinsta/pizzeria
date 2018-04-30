cd9c4bbd149345c57272d8b10f852af751e030b6
pzinsta <voodooshake@gmail.com>
2018-05-01 00:52:39 +0300

    [R] Introduce maven dependency management

--- util/src/main/java/pzinsta/pizzeria/util/Utils.java
+++ util/src/main/java/pzinsta/pizzeria/util/Utils.java
@@ -1,13 +1,4 @@
 package pzinsta.pizzeria.util;
 
-import java.math.BigDecimal;
-
-import javax.money.MonetaryAmount;
-
-import org.javamoney.moneta.Money;
-
 public class Utils {
-	public static MonetaryAmount fromBigDecimal(BigDecimal bigDecimal) {
-		return Money.of(bigDecimal, "USD");
-	}
 }
