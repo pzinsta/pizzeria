696f12c5ca34e0fc14005a682480aa62d6665f87
pzinsta <voodooshake@gmail.com>
2018-05-19 22:26:39 +0300

    [R] Replace the try-catch block with an @ExceptionHandler method to deal with an IOException (sonar)

--- webapp/src/main/java/pzinsta/pizzeria/web/controller/FileStorageController.java
+++ webapp/src/main/java/pzinsta/pizzeria/web/controller/FileStorageController.java
@@ -2,10 +2,13 @@ package pzinsta.pizzeria.web.controller;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import pzinsta.pizzeria.service.FileStorageService;
 
 import javax.servlet.http.HttpServletResponse;
@@ -25,13 +28,15 @@ public class FileStorageController {
     }
 
     @GetMapping
-    public void getFile(@PathVariable("name") String name, HttpServletResponse httpServletResponse) {
+    public void getFile(@PathVariable("name") String name, HttpServletResponse httpServletResponse) throws IOException {
         String contentType = fileStorageService.getContentTypeByName(name).orElse(APPLICATION_OCTET_STREAM_VALUE);
         httpServletResponse.setContentType(contentType);
-        try {
-            IOUtils.copy(fileStorageService.getFileAsInputStream(name), httpServletResponse.getOutputStream());
-        } catch (IOException e) {
-        }
+        IOUtils.copy(fileStorageService.getFileAsInputStream(name), httpServletResponse.getOutputStream());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void handleIOException() {
     }
 
     public FileStorageService getFileStorageService() {
